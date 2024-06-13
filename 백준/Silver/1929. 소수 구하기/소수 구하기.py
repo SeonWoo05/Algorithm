7cbe29179d3e8abb5db12ffc35b51b06a50318a9
SeonWoo05 <164982026+SeonWoo05@users.noreply.github.com>
2024-06-13 12:12:41 +0900

    [Silver III] Title: 소수 구하기, Time: 3184 ms, Memory: 66652 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,9 +1,21 @@
+def find_sosu(a,b):
+    C = []
+    for i in range(a,b+1):
+        if i == 1:
+            continue
+        else:
+            C.append(i)
+    B = []
+
+    for i in C:
+        for j in range(2,int(i**(1/2))+1):
+            if i % j == 0:
+                break
+        else:
+            B.append(i)
+    return B
+                
+
 a,b = map(int, input().split())
-for i in range(a,b+1):
-    if i == 1: # 1은 소수가 아니므로 제거
-        continue
-    for j in range(2,int(i**(1/2))+1):
-        if i % j == 0: # 자신의 양의 제곱근 이하의 배수만 다 제거
-            break # 약수 있으면 바로 for문 탈출
-    else:
-        print(i)
+for i in find_sosu(a,b):
+    print(i) 
