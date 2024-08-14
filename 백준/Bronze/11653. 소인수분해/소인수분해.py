f5cae58b8c8315ec2d06640a9d1d8ee7f33d350b
SeonWoo05 <164982026+SeonWoo05@users.noreply.github.com>
2024-08-14 23:22:23 +0900

    [Bronze I] Title: 소인수분해, Time: 1116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,15 +1,10 @@
 N = int(input())
 
-A = []
-for i in range(2,N+1):
-    A.append(i)
+if N == 1:
+    print('')
 
-B = []
-for i in A:
+for i in range(2, N+1):
     if N % i == 0:
         while N % i == 0:
+            print(i)
             N = N / i
-            B.append(i)
-
-for i in B:
-    print(i)
