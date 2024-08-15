9ca257d7eb935ec35c7097697b660adae5a47e9f
SeonWoo05 <164982026+SeonWoo05@users.noreply.github.com>
2024-08-15 15:25:19 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,9 +1,7 @@
-import math
+A,B,V = map(int, input().split())
 
-A, B, V = map(int, input().split())
-
-# 목표 지점에 도달하기 전까지 남은 거리를 (A-B)로 나누어 몇 일이 걸리는지 계산
-# 마지막 날에는 미끄러지지 않으므로 A만큼 이동한 후 목표에 도달
-days = math.ceil((V - B) / (A - B))
+days = (V-B) // (A-B) # 올라가서 도착하면 끝 => V-B
+if ((V-B) % (A-B)) != 0:
+    days += 1
 
 print(days)
