62879fc751ce1c67b16519265c607041e8967b3b
SeonWoo05 <164982026+SeonWoo05@users.noreply.github.com>
2024-11-13 17:02:34 +0900

    [Silver I] Title: 완전 이진 트리, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -0,0 +1,21 @@
+def tree_by_level(K,values):
+    def build_tree(level,start,end):
+        if level >= K or start > end:
+            return
+        
+        mid = (start + end) // 2
+        levels[level].append(values[mid])
+
+        build_tree(level+1,start,mid-1)
+        build_tree(level+1,mid+1,end)
+
+    levels = [[] for _ in range(K)]
+    build_tree(0,0,len(values)-1)
+    return levels
+
+K = int(input())
+values = list(map(int, input().split()))
+
+result = tree_by_level(K,values)
+for level in result:
+    print(" ".join(map(str, level)))
